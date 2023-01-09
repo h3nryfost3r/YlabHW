c03a4c88047c5de27567238acf34622a99b4519c
h3nryfost3r <henry.foster71@yahoo.com>
2023-01-09 20:28:06 +0300

    Done 4 tasks

--- HW1/task3.py
+++ HW1/task3.py
@@ -1,13 +1,16 @@
 def zeros(n):
-    n10 = n2 = n5 = 0
+    n2 = n5 = n10 = 0
     for num in range(1, n+1):
-        if num % 10 == 0:
-            n10 += num // 10
-            continue
-        if num % 2 == 0:
-            n2 += num // 2
-        if num % 5 == 0:
-            n5 += num // 5
+        while num % 10 == 0:
+            n10 += 1
+            num //= 10
+        while num % 2 == 0:
+            n2 += 1
+            num //= 2
+        while num % 5 == 0:
+            n5 += 1
+            num //= 5
+    return n10 + min(n2, n5)
 
 assert zeros(0) == 0
 assert zeros(6) == 1
---
+++ HW1/task4.py
@@ -0,0 +1,24 @@
+"""
+Написать метод bananas, который принимает на вход строку и возвращает количество слов «banana» в строке.
+
+(Используйте - для обозначения зачеркнутой буквы)
+"""
+
+import itertools as it
+
+def bananas(s):
+    result = set()
+    maskPermutations = { x for x in it.permutations([1]*len('banana') + [0]*(len(s)-len('banana'))) }
+    for mask in maskPermutations:
+        if ''.join([x for x in it.compress(s, mask)]) == 'banana':
+            result.add(''.join(list(map(lambda w, m: w if m else '-', list(s), mask))))
+    return result
+
+assert bananas("banann") == set()
+assert bananas("banana") == {"banana"}
+assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+                     "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+                     "-ban--ana", "b-anana--"}
+assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
+
